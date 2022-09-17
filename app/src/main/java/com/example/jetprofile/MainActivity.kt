25503484c0992d48027a6954efeb35ba0a83fb3c
masato1230 <0ht3851f11t426k@ezweb.ne.jp>
2022-09-17 22:53:22 +0900

    7_add_show_detail_button

--- app/src/main/java/com/example/jetprofile/MainActivity.kt
+++ app/src/main/java/com/example/jetprofile/MainActivity.kt
@@ -95,6 +95,16 @@ class MainActivity : ComponentActivity() {
                                 thickness = 2.dp,
                             )
                         }
+                        Spacer(modifier = Modifier.height(20.dp))
+
+                        // 詳細表示ボタン
+                        Button(
+                            modifier = Modifier.fillMaxWidth(),
+                            colors = ButtonDefaults.buttonColors(backgroundColor = Color(0xFFF85F6A)),
+                            onClick = { /*TODO*/ },
+                        ) {
+                            Text(text = "詳細を表示", color = Color.White)
+                        }
                     }
                 }
             }
