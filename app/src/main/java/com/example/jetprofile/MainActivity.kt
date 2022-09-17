75fc7b55ff0a49cf9e3cdf3e333e049d199bf690
masato1230 <0ht3851f11t426k@ezweb.ne.jp>
2022-09-17 12:35:23 +0900

    5_add_email

--- app/src/main/java/com/example/jetprofile/MainActivity.kt
+++ app/src/main/java/com/example/jetprofile/MainActivity.kt
@@ -6,9 +6,9 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Email
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -74,6 +74,25 @@ class MainActivity : ComponentActivity() {
                                 text = "DXカンパニー テクノロジーグループ",
                                 color = Color.Gray,
                                 fontSize = 16.sp,
+                            )
+                            Spacer(modifier = Modifier.height(20.dp))
+
+                            // Email
+                            Row(verticalAlignment = Alignment.CenterVertically) {
+                                Icon(
+                                    imageVector = Icons.Default.Email,
+                                    contentDescription = "email",
+                                )
+                                Spacer(modifier = Modifier.width(10.dp))
+                                Text(text = "Email", fontSize = 14.sp, fontWeight = FontWeight.Bold)
+                            }
+                            Spacer(modifier = Modifier.height(10.dp))
+                            Text(text = "example@ezweb.ne.jp", fontSize = 16.sp)
+                            Spacer(modifier = Modifier.height(5.dp))
+
+                            Divider(
+                                modifier = Modifier.clip(RoundedCornerShape(1000.dp)),
+                                thickness = 2.dp,
                             )
                         }
                     }
