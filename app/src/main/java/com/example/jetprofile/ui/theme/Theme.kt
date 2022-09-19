3026575950442353d0b1a413f2042e9e4af4ca8a
masato1230 <0ht3851f11t426k@ezweb.ne.jp>
2022-09-19 10:50:16 +0900

    12_custom_bar_color

--- app/src/main/java/com/example/jetprofile/ui/theme/Theme.kt
+++ app/src/main/java/com/example/jetprofile/ui/theme/Theme.kt
@@ -5,6 +5,8 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 
 private val DarkColorPalette = darkColors(
     primary = Purple200,
@@ -34,6 +36,11 @@ fun JetProfileTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Compos
     } else {
         LightColorPalette
     }
+
+    rememberSystemUiController().setSystemBarsColor(
+        color = if (darkTheme) Color.Black else Color.White,
+        darkIcons = !darkTheme,
+    )
 
     MaterialTheme(
         colors = colors,
